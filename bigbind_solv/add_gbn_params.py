812df142ab93188306bcdb4ad5218b720de25808
Michael Arcidiacono <mixarcidiacono@gmail.com>
2024-08-05 17:49:50 -0400

    smh this is v annoying

--- bigbind_solv/add_gbn_params.py
+++ bigbind_solv/add_gbn_params.py
@@ -8,23 +8,17 @@ from openmm.app.internal.customgbforces import GBSAGBn2Force
 from datasets.md_batch import *
 from bigbind_solv.gb_baseline import *
 from traceback import print_exc
-import signal
+import multiprocessing as mp
 
 
-class timeout:
-    def __init__(self, seconds, error_message="Timeout"):
-        self.seconds = seconds
-        self.error_message = error_message
-
-    def handle_timeout(self, signum, frame):
-        raise TimeoutError(self.error_message)
-
-    def __enter__(self):
-        signal.signal(signal.SIGALRM, self.handle_timeout)
-        signal.alarm(self.seconds)
-
-    def __exit__(self, type, value, traceback):
-        signal.alarm(0)
+def make_openmm_topology(data, ret_queue):
+    try:
+        ret_queue.put(to_openmm_topology(data))
+    except KeyboardInterrupt:
+        raise
+    except:
+        print_exc()
+        ret_queue.put(None)
 
 def add_gbn_params():
     os.makedirs(CONFIG.bigbind_solv_dir, exist_ok=True)
@@ -40,22 +34,38 @@ def add_gbn_params():
         print("Adding Gbn parameters to", split)
         for index in trange(len(dataset)):
             try:
-                with timeout(5):
-                    key = dataset.keys[index]
+                key = dataset.keys[index]
 
-                    data = dataset[index]
-                    topology = to_openmm_topology(data)
-                    force = GBSAGBn2Force(cutoff=None,SA="ACE")
-                    gbn2_params = force.getStandardParameters(topology).shape
+                data = dataset[index]
 
-                    out_group = h5_file.create_group(key)
-                    in_group = dataset.file[key]
-                    for k, v in in_group.items():
-                        out_group[k] = v[()]
-                    out_group["gbn2_params"] = gbn2_params
+                # annoying way to make sure this times out after 1 sec
+                q = mp.Queue()
+                p = mp.Process(target=make_openmm_topology, args=(data,q))
+                p.start()
+                p.join(5)
+                if p.is_alive():
+                    p.terminate()
+                    p.join()
+                    print("Timeout for index", index)
+                    continue
+
+                topology = q.get()
+                if topology is None:
+                    print("Error at index", index)
+                    continue
+
+                force = GBSAGBn2Force(cutoff=None,SA="ACE")
+                gbn2_params = force.getStandardParameters(topology).shape
+
+                out_group = h5_file.create_group(key)
+                in_group = dataset.file[key]
+                for k, v in in_group.items():
+                    out_group[k] = v[()]
+                out_group["gbn2_params"] = gbn2_params
             except KeyboardInterrupt:
                 raise
             except:
+                print("Error at index", index)
                 print_exc()
         
         h5_file.close()
